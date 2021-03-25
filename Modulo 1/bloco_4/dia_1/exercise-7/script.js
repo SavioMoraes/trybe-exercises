6986260d60d390bb3f5f74e41026d37c8bbe5392
Savio Moraes <smmsavio@hotmail.com>
2021-03-25 20:56:09 -0300

    Resolve exercicio 7

---
+++ Modulo 1/bloco_4/dia_1/exercise-7/script.js
@@ -0,0 +1,19 @@
+let testGrade = 100;
+
+if (testGrade > 100) {
+  console.log('[ERROR!!]')
+} else if (testGrade >= 90) {
+  console.log('A')
+} else if (testGrade >= 80) {
+  console.log('B')
+} else if (testGrade >= 70) {
+  console.log('')
+} else if (testGrade >= 60) {
+  console.log('D')
+} else if (testGrade >= 50) {
+  console.log('E')
+} else if (testGrade < 50) {
+  console.log('F')
+} if (testGrade < 0) {
+  console.log('[ERROR!!]')
+}
