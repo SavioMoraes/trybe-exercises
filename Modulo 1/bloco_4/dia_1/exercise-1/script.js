d4428b6da9ae0e686ac932624b0b403675842292
Savio Moraes <smmsavio@hotmail.com>
2021-03-25 13:37:39 -0300

    Resolve Exercicio 1

---
+++ Modulo 1/bloco_4/dia_1/exercise-1/script.js
@@ -0,0 +1,22 @@
+let a = 5;
+let b = 2
+
+// Adição
+let soma = a + b;
+console.log(soma);
+
+// Subtração
+let subtracao = a - b;
+console.log(subtracao);
+
+// Multiplicação
+let mult = a * b;
+console.log(mult);
+
+// Divisão
+let div = a / b;
+console.log(div);
+
+//Módulo
+let mod = a % b;
+console.log(mod);
