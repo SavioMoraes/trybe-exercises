94ba552316000f581077b4079f3856264bf8d95c
Savio Moraes <smmsavio@hotmail.com>
2021-03-24 21:07:30 -0300

    Resolve exericicio 1 do conteudo - JavaScript - Primeiros Passos

---
+++ Modulo 1/bloco_4/dia_1/conteudo-01/script.js
@@ -0,0 +1,11 @@
+const name = 'Sávio Moreira de Moraes';
+const birthCity = 'Niterói';
+let birthYear = 1980;
+console.log(name);
+console.log(birthCity);
+console.log(birthYear);
+birthYear = 2020;
+console.log(birthYear);
+birthCity = 'Itajaí';
+
+// Erro na linha 9: tentativa de mudança de valor de constante
