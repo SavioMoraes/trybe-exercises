0cad903fad2ad8a0d792a2ba75eb3b0e7976a880
Savio Moraes <smmsavio@hotmail.com>
2021-03-25 21:08:26 -0300

    Resolve exercicio 9

---
+++ Modulo 1/bloco_4/dia_1/exercise-9/script.js
@@ -0,0 +1,9 @@
+let number1 = 2;
+let number2 = 22;
+let number3 = 222;
+
+if (number1 % 2 === 1 || number2 % 2 === 1 || number3 % 2 === 1) {
+  console.log(true);
+} else {
+  console.log(false);
+}
