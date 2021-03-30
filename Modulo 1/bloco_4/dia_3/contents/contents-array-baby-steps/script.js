99fc0333b6411bd17d9e716fffe11e31a1e1f1b0
Savio Moraes <smmsavio@hotmail.com>
2021-03-30 12:32:36 -0300

    Cria diretorio contents-array-baby-steps e adiciona script.js ja com o baby steps

---
+++ Modulo 1/bloco_4/dia_3/contents/contents-array-baby-steps/script.js
@@ -0,0 +1,20 @@
+// Exercício 1
+/*
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
+
+console.log('Exercício 1 - Resposta: ' + menuServices + '.');
+*/
+// Exercício 2
+/*
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu[2];
+
+console.log('Exercício 2 - Resposta: ' + indexOfPortfolio + '.');
+*/
+// Exercício 3
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+menu.push('Contato');
+
+console.log(menu);
