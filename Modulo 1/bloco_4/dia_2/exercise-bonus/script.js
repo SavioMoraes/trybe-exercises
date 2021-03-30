265ef7d3c606d9b94eb3164d35ea876485442097
Savio Moraes <smmsavio@hotmail.com>
2021-03-29 22:20:16 -0300

    Resolve exercise bonus

---
+++ Modulo 1/bloco_4/dia_2/exercise-bonus/script.js
@@ -0,0 +1,44 @@
+// Bônus - Exercise 1
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+/*
+for (let index = 1; index < numbers.length; index += 1) {
+  for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+    if (numbers[index] < numbers[secondIndex]) {
+      let position = numbers[index];
+      numbers[index] = numbers[secondIndex];
+      numbers[secondIndex] = position;
+    }
+  }
+}
+
+console.log(numbers);
+*/
+
+// Bônus - Exercise 2
+/*
+for (let index = 1; index < numbers.length; index += 1) {
+  for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+    if (numbers[index] > numbers[secondIndex]) {
+      let position = numbers[index];
+      numbers[index] = numbers[secondIndex];
+      numbers[secondIndex] = position;
+    }
+  }
+}
+
+console.log(numbers);
+*/
+
+let newArray = [];
+
+for (let index = 0; index < numbers.length; index += 1) {
+  if (numbers[index + 1]) {
+    newArray.push(numbers[index] * numbers[index + 1]);
+  } else {
+    newArray.push(numbers[index] * 2);
+  }
+}
+
+console.log(newArray);
