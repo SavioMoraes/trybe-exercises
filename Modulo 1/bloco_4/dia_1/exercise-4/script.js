936aa7e132dade91c4fe64012db21c52b46d3cea
Savio Moraes <smmsavio@hotmail.com>
2021-04-03 22:58:08 -0300

    Adiciona exercise-4 em contents-function

--- Modulo 1/bloco_4/dia_1/exercise-4/script.js
+++ Modulo 1/bloco_4/dia_1/exercise-4/script.js
@@ -1,3 +1,6 @@
+/*Faça um programa que, dado um valor definido numa variável, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.*/
+
+
 let anyValue = 1;
 
 if (anyValue > 0) {
