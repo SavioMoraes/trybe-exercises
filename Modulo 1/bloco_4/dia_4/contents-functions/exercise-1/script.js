9162ebc7b11248fe9ab26b94df809fa748f1adc5
Savio Moraes <smmsavio@hotmail.com>
2021-04-03 22:03:47 -0300

    Adiciona exercise-1 em contents-functions

---
+++ Modulo 1/bloco_4/dia_4/contents-functions/exercise-1/script.js
@@ -0,0 +1,29 @@
+// Adição Função
+function sum(a, b) {
+  return a + b;
+}
+console.log(sum(3, 8));
+
+// Subtração Função
+function subtraction(a, b) {
+  return a - b;
+}
+console.log(subtraction(200, 120));
+
+// Multiplicação Função
+function multiplication(a, b) {
+  return a * b;
+}
+console.log(multiplication(8, 5));
+
+// Divisão Função
+function division(a, b) {
+  return a / b;
+}
+console.log(division(81, 9));
+
+//Módulo Função
+function module(a, b) {
+  return a % b;
+}
+console.log(module(4, 2));
