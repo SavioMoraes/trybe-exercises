d2a7ff725dc95c6501fb91c4bebb38a5a379678f
Savio Moraes <smmsavio@hotmail.com>
2021-04-04 20:16:30 -0300

    Adiciona primeira parte do conteudo parte II

---
+++ Modulo 1/bloco_5/dia_1/contents-partII-element-selectors/script.js
@@ -0,0 +1,22 @@
+let paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red";
+
+let title = document.getElementById('page-title');
+title.innerText = "O Hobbit: Uma Jornada Inesperada";
+
+let secondParagraph = document.getElementById('second-paragraph');
+secondParagraph.style.backgroundColor = 'rgba(0, 0, 0, 0.8)';
+secondParagraph.style.color = 'yellowgreen';
+secondParagraph.style.width = '33%';
+secondParagraph.style.padding = '3%';
+secondParagraph.style.textAlign = 'center';
+secondParagraph.style.textTransform = "uppercase";
+
+let subTitle = document.getElementById('subtitle');
+subTitle.style.backgroundColor = 'rgba(255, 0, 0, 0.7)';
+subTitle.style.color = 'black';
+subTitle.style.width = '50%';
+subTitle.style.padding = '5%';
+subTitle.style.textAlign = 'center';
+subTitle.style.textTransform = 'uppercase';
+subTitle.style.fontSize = '2em';
