714ba3e7244ce3a8aeb1a2fe1e32a018ac6f0318
Savio Moraes <smmsavio@hotmail.com>
2021-04-06 21:43:44 -0300

    Conserta script.js

--- Modulo 1/bloco_5/dia_2/contents-partI-seeking-elements/script.js
+++ Modulo 1/bloco_5/dia_2/contents-partI-seeking-elements/script.js
@@ -1,25 +1,25 @@
-// Exercício 1
-document.querySelector('#elementoOndeVoceEsta');
+// // Exercício 1
+console.log(document.querySelector('#elementoOndeVoceEsta'));
 
 // Exercício 2
-document.querySelector('#elementoOndeVoceEsta').parentNode.style.color = 'blue';
+console.log(document.querySelector('#elementoOndeVoceEsta').parentNode.style.color = 'blue');
 
 // Exercício 3
-document.querySelector('#elementoOndeVoceEsta').firstElementChild.innerHTML = 'Prazer, sou o primeiro filho!';
+console.log(document.querySelector('#elementoOndeVoceEsta').firstElementChild.innerHTML = 'Prazer, sou o primeiro filho!');
 // se for utilizado o 'firstChild', o resultado é apresentado apenas no console, sem acrescentar texto a página do navegador
 
 // Exercício 4
-document.querySelector('#pai').firstElementChild; 
+console.log(document.querySelector('#pai').firstElementChild); 
 
 // Exercício 5
-document.querySelector('#elementoOndeVoceEsta').previousElementSibling;
+console.log(document.querySelector('#elementoOndeVoceEsta').previousElementSibling);
 
 // Exercise 6
-document.querySelector('#elementoOndeVoceEsta').nextSibling;
+console.log(document.querySelector('#elementoOndeVoceEsta').nextSibling);
 // se for utilizado o 'nextElementSbling' ele acessará a próxima 'div', porque busca por elementos e não por nós!
 
 // Exercise 7
-document.querySelector('#elementoOndeVoceEsta').nextElementSibling;
+console.log(document.querySelector('#elementoOndeVoceEsta').nextElementSibling);
 
 // Exercise 8
-document.querySelector('#pai').firstElementChild.nextElementSibling.nextElementSibling;
+console.log(document.querySelector('#pai').firstElementChild.nextElementSibling.nextElementSibling);
