744f3f699d76083a9006568a42041afb40fdaa34
Savio Moraes <smmsavio@hotmail.com>
2021-04-08 13:44:16 -0300

    Adiciona exercise-7

--- Modulo 1/bloco_5/dia_3/exercises-5.3-events/script.js
+++ Modulo 1/bloco_5/dia_3/exercises-5.3-events/script.js
@@ -154,3 +154,18 @@ function dayMouseOut() {
 
 dayMouseOver();
 dayMouseOut();
+
+/*
+  Exercício 7: Implemente uma função que adiciona uma tarefa personalizada ao calendário. A função deve receber como parâmetro a string com o nome da tarefa (ex: "cozinhar") e criar dinamicamente um elemento com a tag <span> contendo a tarefa.
+    -O elemento criado deverá ser adicionado como filho/filha da tag <div> que possui a classe "my-tasks".
+*/
+function newTaskSpan(task) {
+
+  let tasksContainer = document.querySelector('.my-tasks');
+  let taskName = document.createElement('span');
+
+  taskName.innerHTML = task;
+  tasksContainer.appendChild(taskName);
+};
+
+newTaskSpan('Projeto:');
