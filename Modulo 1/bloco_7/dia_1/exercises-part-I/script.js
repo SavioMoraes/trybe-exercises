3a7f47c4869a366cd6adecd95fb8fc5e1dbe355e
Savio Moraes <smmsavio@hotmail.com>
2021-04-27 10:55:20 -0300

    Adiciona bonus do exercicio 2 parte 1 do bloco 7.1

--- Modulo 1/bloco_7/dia_1/exercises-part-I/script.js
+++ Modulo 1/bloco_7/dia_1/exercises-part-I/script.js
@@ -30,3 +30,21 @@ const sortOddsAndEvens = () => {
 
 const sortedArray = sortOddsAndEvens();
 console.log(`Os números ${sortedArray} se encontram ordenados de forma crescente !`);
+
+// Bônus - array.sort()
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const sortArrayBonus = array => {
+  const sortOddsAndEvens = array.sort((a, b) => a - b);
+  return sortOddsAndEvens;
+}
+
+const sortedArrayBonus = sortArrayBonus(oddsAndEvens);
+console.log(`Os números ${sortedArrayBonus} se encontram ordenados de forma crescente !`);
+
+// Bônus - array.sort com sort em uma linha...
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+console.log(`Os números ${oddsAndEvens.sort((a, b) => a - b)} se encontram ordenados de forma crescente !`);
