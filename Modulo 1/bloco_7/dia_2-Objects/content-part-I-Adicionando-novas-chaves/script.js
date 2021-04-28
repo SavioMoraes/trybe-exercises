61ac1e9f8b1382a19bdabc52f9f496290bdf4959
Savio Moraes <smmsavio@hotmail.com>
2021-04-28 10:14:41 -0300

    Anota duvida na pratica do conteudo

--- Modulo 1/bloco_7/dia_2-Objects/content-part-I-Adicionando-novas-chaves/script.js
+++ Modulo 1/bloco_7/dia_2-Objects/content-part-I-Adicionando-novas-chaves/script.js
@@ -48,3 +48,5 @@ console.log(customer);
 
 // Resolução da prática aqui
 // Agora, para praticar, crie uma função que receba três parâmetros, sendo eles: um objeto, o nome de uma chave e o seu valor. O retorno dessa função deve ser o objeto já com a nova chave adicionada nele
+
+// Dificuldade aqui!!!
