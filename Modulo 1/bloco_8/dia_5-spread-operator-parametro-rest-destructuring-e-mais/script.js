01bf321358aad0519fd40bf1aca14bac001c1c1c
Savio Moraes <smmsavio@hotmail.com>
2021-05-06 15:28:21 -0300

    Adiciona algumas praticas...

---
+++ Modulo 1/bloco_8/dia_5-spread-operator-parametro-rest-destructuring-e-mais/script.js
@@ -0,0 +1,40 @@
+// const person = {
+//   name: 'João',
+//   lastName: 'Jr',
+//   age: 34,
+// };
+
+// const { nationality = 'Brazilian' } = person;
+// console.log(nationality); // Brazilian
+// console.log(person); // nationality não é adicionado ao array person!
+
+// const position2d = [1.0, 2.0];
+// const [x, y, z = 0] = position2d;
+
+// console.log(x); // 1
+// console.log(y); // 2
+// console.log(z); // 0
+
+// const getNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
+
+// let person = {
+//     firstName: 'João',
+//     lastName: 'Jr II',
+// };
+
+// let {firstName, lastName, nationality = 'Brazilian',} = person; 
+
+// const otherPerson = {
+//     firstName: 'Ivan',
+//     lastName: 'Ivanovich',
+//     nationality: 'Russian',
+// };
+
+// console.log(getNationality(otherPerson)); // Ivan is Russian
+// console.log(getNationality(person)); // não consegui
+
+const getPosition = (latitude, longitude) => ({
+  latitude,
+  longitude});
+
+console.log(getPosition(-19.8157, -43.9542));
