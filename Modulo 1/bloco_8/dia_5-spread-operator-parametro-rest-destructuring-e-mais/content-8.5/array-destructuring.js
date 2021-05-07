fac424e1b5cdb0a568094359cbaeae3eecbacb89
Savio Moraes <smmsavio@hotmail.com>
2021-05-07 00:18:06 -0300

    Finaliza array-destructuring.js :heavy_check_mark:

--- Modulo 1/bloco_8/dia_5-spread-operator-parametro-rest-destructuring-e-mais/content-8.5/array-destructuring.js
+++ Modulo 1/bloco_8/dia_5-spread-operator-parametro-rest-destructuring-e-mais/content-8.5/array-destructuring.js
@@ -1,11 +1,27 @@
 const arrayCountries = ['Brazil', 'Japan', 'China', 'Canada'];
 
-const firstCountry = arrayCountries[0];
-const secondCountry = arrayCountries[1];
-const thirdCountry = arrayCountries[2];
-const fourthCountry = arrayCountries[3];
+// const firstCountry = arrayCountries[0];
+// const secondCountry = arrayCountries[1];
+// const thirdCountry = arrayCountries[2];
+// const fourthCountry = arrayCountries[3];
+
+//---------------------------------------------------------------------------
+
+const [firstCountry, secondCountry, thirdCountry, fourthCountry] = arrayCountries;
 
 console.log(firstCountry); // Brazil
 console.log(secondCountry); // Japan
 console.log(thirdCountry); // China
 console.log(fourthCountry); // Canada
+
+//---------------------------------------------------------------------------
+
+const saudacoes = ['Olá', (saudacao) => console.log(saudacao)];
+
+// console.log(saudacoes[1](saudacoes[0])); // Olá
+
+// Produza o mesmo resultado acima, porém utilizando array destructuring
+
+const [saudacao, realizaSaudacao] = saudacoes;
+
+console.log (realizaSaudacao(saudacao));
