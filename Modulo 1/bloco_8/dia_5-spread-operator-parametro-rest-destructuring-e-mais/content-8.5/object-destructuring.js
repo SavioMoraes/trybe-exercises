d57699ac4df26c30baf48cd3444c005c785f39e6
Savio Moraes <smmsavio@hotmail.com>
2021-05-06 23:35:21 -0300

    Finaliza 0bject-destructuring.js :heavy_check_mark:

--- Modulo 1/bloco_8/dia_5-spread-operator-parametro-rest-destructuring-e-mais/content-8.5/object-destructuring.js
+++ Modulo 1/bloco_8/dia_5-spread-operator-parametro-rest-destructuring-e-mais/content-8.5/object-destructuring.js
@@ -3,3 +3,58 @@ const product = {
   price: '1899.05',
   seller: 'Casas de Minas',
 };
+
+// console.log(product.name);
+// console.log(product['name']);
+// console.log(product.price);
+// console.log(product.seller);
+
+//--------------------------------------------------------------------------
+
+// const { name } = product;
+// console.log(name); // Smart TV Crystal UHD
+
+//--------------------------------------------------------------------------
+
+const { name, seller } = product;
+console.log(name); // Smart TV Crystal UHD
+console.log(seller); // Casas de Minas
+
+//--------------------------------------------------------------------------
+
+const student = {
+  a: 'Maria',
+  b: 'Turma B',
+  c: 'Matematica',
+};
+
+const { a: name, b: classAssigned, c: subject, d: address } = student;
+
+console.log(name); // Maria
+console.log(classAssigned); // Turma B
+console.log(subject); // Matemática
+console.log(address); // undefined -- OK!
+
+//--------------------------------------------------------------------------
+
+// const student = {
+//   a: 'Maria',
+//   b: 'Turma B',
+//   c: 'Matematica',
+// };
+// const name = student.a;
+// console.log(name); // Maria
+
+//---------------------------------------------------------------------------
+
+const product = {
+  name: 'Smart TV Crystal UHD',
+  price: '1899.05',
+  seller: 'Casas de Minas',
+};
+
+const printProductDetails = ({ name, price, seller }) => {
+  console.log(`Promoção! ${name} por apenas ${price} é só aqui: ${seller}`);
+};
+
+printProductDetails(product); // Promoção! Smart TV Crystal UHD por apenas 1899.05 é só aqui: Casas de Minas
