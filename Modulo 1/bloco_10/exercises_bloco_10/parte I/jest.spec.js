e08b7b2a530f3ef76161e3ea30ca38245c0d1be1
Savio Moraes <smmsavio@hotmail.com>
2021-05-19 15:21:19 -0300

    agora entendi: e o nome da constante e nao do arquivo chamado que colocca antes da function

--- Modulo 1/bloco_10/exercises_bloco_10/parte I/jest.spec.js
+++ Modulo 1/bloco_10/exercises_bloco_10/parte I/jest.spec.js
@@ -1,18 +1,18 @@
-const { sum } = require('./exercicio01');
+const sum = require('./exercicio01');
 const { test, expect, describe } = require('@jest/globals');
 
 describe('a função sum', () => {
   test('sum is function', () => {
-    expect(typeof sum).toBe('function');
+    expect(typeof sum.sum).toBe('function');
   });
   test('sums two values', () => {
-    expect(sum(2, 3)).toEqual(5);
+    expect(sum.sum(2, 3)).toEqual(5);
   });
   it('sums two values', () => {
-    expect(sum(4, 5)).toEqual(9);
+    expect(sum.sum(4, 5)).toEqual(9);
   });
   it('sums two values', () => {
-    expect(sum(0, 0)).toEqual(0);
+    expect(sum.sum(0, 0)).toEqual(0);
   });
 });
 
