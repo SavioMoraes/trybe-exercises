f4ca9f31547d3695cc5f76f1fe8cd4e9ee619316
Savio Moraes <smmsavio@hotmail.com>
2021-06-12 13:10:25 -0300

    hello wordl no react :rocked:

---
+++ Modulo 2/Bloco_11.1-Introdução à React/dia_1-Introdução-front-end-react-hello-world/exampla_video_npm/my_app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>HELLO WORLD NO REACT</h1>
+      <p>Agora a maldição vai por terra e vou conseguir prender essa jossa!!!</p>
+    </div>
+  );
+}
+
+export default App;
