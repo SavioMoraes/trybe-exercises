24a49e4dcd104a493c70a250a8b4346f49f3622e
Savio Moraes <smmsavio@hotmail.com>
2021-06-12 14:07:01 -0300

    criando primeiro componente

--- Modulo 2/Bloco_11.1-Introdução à React/dia_1-Introdução-front-end-react-hello-world/exampla_video_npm/my_app/src/App.js
+++ Modulo 2/Bloco_11.1-Introdução à React/dia_1-Introdução-front-end-react-hello-world/exampla_video_npm/my_app/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Hello from './components/Hello';
 
 function App() {
   return (
     <div className="App">
       <h1>HELLO WORLD NO REACT</h1>
       <p>Agora a maldição vai por terra e vou conseguir prender essa jossa!!!</p>
+      <Hello />
     </div>
   );
 }
