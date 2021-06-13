4bf830028f011b72e9cbbb4540db3c5402552ca9
Savio Moraes <smmsavio@hotmail.com>
2021-06-13 14:17:36 -0300

    coloca o bind dentro do construcos para dar acesso ao this dentro da function

--- Modulo 2/Bloco_12-Componentes-com-Estado-Eventos-e-Formulários-com-React/dia_1-Componentes com estados e eventos/event-button/example-event-button/src/App.js
+++ Modulo 2/Bloco_12-Componentes-com-Estado-Eventos-e-Formulários-com-React/dia_1-Componentes com estados e eventos/event-button/example-event-button/src/App.js
@@ -1,25 +1,38 @@
 import React from 'react';
 import './App.css';
 
-function handleClickSavio() {
-  console.log('Clicou em Sávio')
-}
-
-function handleClickNoah() {
-  console.log('Clicou na Noah!')
-}
-
-function handleClickThor() {
-  console.log('Clicou no Thorzinho!')
-}
-
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log('componene sendo construído');
+
+    this.handleClickSavio = this.handleClickSavio.bind(this);
+    this.handleClickNoah = this.handleClickNoah.bind(this);
+    this.handleClickThor = this.handleClickThor.bind(this);
+  }
+  
+  handleClickSavio() {
+    // console.log(this);
+    console.log('Clicou em Sávio')
+  }
+  
+  handleClickNoah() {
+    // console.log(this);
+    console.log('Clicou na Noah!')
+  }
+  
+  handleClickThor() {
+    // console.log(this);
+    console.log('Clicou no Thorzinho!')
+  }
+
   render() {
+    // console.log(this);
     return (
       <div>
-        <button onClick={handleClickSavio}>Sávio</button>
-        <button onClick={handleClickNoah}>Noah</button>
-        <button onClick={handleClickThor}>Thor</button>
+        <button onClick={this.handleClickSavio}>Sávio</button>
+        <button onClick={this.handleClickNoah}>Noah</button>
+        <button onClick={this.handleClickThor}>Thor</button>
       </div>
     );
   }
