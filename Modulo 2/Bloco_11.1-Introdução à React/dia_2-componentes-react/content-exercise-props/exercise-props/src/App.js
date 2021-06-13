481fbafba5febec65c8e17744ca4d8a595451783
Savio Moraes <smmsavio@hotmail.com>
2021-06-12 23:00:50 -0300

    adiciona exercicio

--- Modulo 2/Bloco_11.1-Introdução à React/dia_2-componentes-react/content-exercise-props/exercise-props/src/App.js
+++ Modulo 2/Bloco_11.1-Introdução à React/dia_2-componentes-react/content-exercise-props/exercise-props/src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
+import Image from './Image';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Image source={ 'https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg' } alt="Cute cat staring" />
     </div>
   );
 }
