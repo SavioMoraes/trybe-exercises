c18758038b4658750c9d10f2993585c6017735ce
Savio Moraes <smmsavio@hotmail.com>
2021-06-13 11:28:04 -0300

    example com proptypes

---
+++ Modulo 2/Bloco_11.1-Introdução à React/dia_2-componentes-react/content-example-proptypes/example-proptypes/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Greeting from './Greeing';
+
+function App() {
+  return (
+    <div className="App">
+        <Greeting name={'Sávio'} lastName={'Moraes'} />
+    </div>
+  );
+}
+
+export default App;
