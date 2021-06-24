0a53c2a39c622620b718bcaaea24eb4c9299414b
Savio Moraes <smmsavio@hotmail.com>
2021-06-24 11:28:28 -0300

    refazzendo e testando conteúdo dia_1 - Introdução à RTL

--- Modulo 2/Bloco_15-testes-automatizados-com-react-testing-library/dia_1-Introdução-ao-RTL/content-intro-RTL/testes-react/src/App.js
+++ Modulo 2/Bloco_15-testes-automatizados-com-react-testing-library/dia_1-Introdução-ao-RTL/content-intro-RTL/testes-react/src/App.js
@@ -1,50 +1,14 @@
-  // App.js
-  import React, { Component } from 'react';
-  import ValidEmail from './components/ValidEmail';
-  import './App.css';
-  
-  class App extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        email: '',
-        saveEmail: '',
-      };
-    }
-  
-    changeEmail(value) {
-      this.setState({ email: value });
-    }
-  
-    changeSaveEmail(value) {
-      this.setState({ saveEmail: value, email: '' });
-    }
-  
-    render() {
-      const { email, saveEmail } = this.state;
-      return (
-        <div className="App">
-          <label htmlFor="id-email">
-            Email
-          </label>
-          <input
-            id="id-email"
-            value={email}
-            type="email"
-            onChange={(e) => this.changeEmail(e.target.value)}
-          />
-          <input
-            id="btn-enviar"
-            type="button"
-            data-testid="id-send"
-            value="Enviar"
-            onClick={() => this.changeSaveEmail(email)}
-          />
-          <input id="btn-id" type="button" value="Voltar" />
-          <ValidEmail email={saveEmail}/>
-        </div>
-      );
-    }
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return(
+      <div className="App">
+        <h1 className="App-header">Olá eu sou o App</h1>
+      </div>
+    )
   }
-  
-  export default App;
+}
+
+export default App;
