bce8160fe9e7269310a75f9074711c9980ba147c
Savio Moraes <smmsavio@hotmail.com>
2021-06-24 22:35:41 -0300

    finaliza conteúdo dia_1 - parte 1

--- Modulo 2/Bloco_15-testes-automatizados-com-react-testing-library/dia_1-Introdução-ao-RTL/content-intro-RTL/testes-react/src/App.test.js
+++ Modulo 2/Bloco_15-testes-automatizados-com-react-testing-library/dia_1-Introdução-ao-RTL/content-intro-RTL/testes-react/src/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import App from './App';
 
 describe('Testa a existência do componente App:', () => {
@@ -22,10 +22,54 @@ describe('Testa a existência do componente App:', () => {
     expect(inputEmail).toBeInTheDocument();
     expect(inputEmail.type).toBe('email');
   });
+
+  it('Verifica que há dois botões', () => {
+    // Acessar os elementos de sua tela
+    const { getAllByRole } = render(<App />);
+    const button = getAllByRole('button');
+
+    // Interagir com eles (se houver necessidade)
+      // Não houve necessidade
+
+    // Fazer seu teste
+    expect(button.length).toBe(2)
+  });
+
+  it('Verifica a existência do botão `Enviar`.', () => {
+    // Acessar os elementos de sua tela
+    const { getByTestId } = render(<App />);
+    const btnEnviar = getByTestId('id-send');
+  
+    // Interagir com eles (se houver necessidade)
+      // Não houve necessidade
+  
+    // Fazer seu teste
+    expect(btnEnviar).toBeInTheDocument();
+    expect(btnEnviar.type).toBe('button');
+    expect(btnEnviar).toHaveValue('Enviar');
+  });
+  
+  it('Verifica se, ao inserir um email e clicar em "Enviar", o email aparece na tela.', () => {
+    // Acessar os elementos de sua tela
+    const { getByTestId, getByLabelText } = render(<App />);
+
+    const EMAIL_USER = 'email@email.com';
+
+    const emailInput = getByLabelText('Email');
+    const sendButton = getByTestId('id-send');
+    const userEmail = getByTestId('id-email-user');
+  
+    // Interagir com eles (se houver necessidade)
+    fireEvent.change(emailInput, { target: { value: EMAIL_USER } });
+    fireEvent.click(sendButton);
+    
+    // Fazer seu teste
+    expect(userEmail).toBeInTheDocument();
+    expect(userEmail).toHaveTextContent('Valor:');
+    expect(emailInput.value).toBe('');
+    expect(userEmail.textContent).toBe(`Valor: ${EMAIL_USER}`)
+  });
+
 });
 
-// Acessar os elementos de sua tela
 
-// Interagir com eles (se houver necessidade)
-
-// Fazer seu teste
