b6247652d697eb7946a522143e90fa0dd3c81351
Savio Moraes <smmsavio@hotmail.com>
2021-06-26 13:21:08 -0300

    sobe exemplo com promise só visualizável no browser, codígo agora será apagado para resolução dos testes

--- Modulo 2/Bloco_15-testes-automatizados-com-react-testing-library/dia_2-RTL-Mocks-e-Inputs/content-dia_2/moking-modules/testing-mock/src/App.test.js
+++ Modulo 2/Bloco_15-testes-automatizados-com-react-testing-library/dia_2-RTL-Mocks-e-Inputs/content-dia_2/moking-modules/testing-mock/src/App.test.js
@@ -6,3 +6,10 @@ const soma = 2 + 2;
 const meuObjetoQualquer2AMissao = { minhaChaveFavorita: () => console.log((`Soma: ${soma}`)) }
 
 meuObjetoQualquer2AMissao.minhaChaveFavorita();
+
+// Este código só funfa no console do inspect do navegador
+/*
+const response = { json: () => Promise.resolve({ joke: 'Whiteboards... are remarkble'})}
+
+response.json();
+*/
