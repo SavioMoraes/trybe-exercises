cbc1ed3128ea2bc9a57376673406ee23435e8c61
Savio Moraes <smmsavio@hotmail.com>
2021-09-25 22:29:23 -0300

    aula ao vivo

---
+++ Modulo 3/Bloco 26/Dia_26.1-Intro_NodeJS/aula-26-1/funcoes.js
@@ -0,0 +1,19 @@
+// function soma(n1, n2) {
+//   return n1 + n2;
+// }
+
+// module.exports = { soma };
+
+function soma(n1, n2) {
+  return n1 + n2;
+}
+
+function sub(n1, n2) {
+  return n1 - n2;
+}
+
+function mult(n1, n2) {
+  return n1 * n2;
+}
+
+module.exports = { soma, sub, mult };
--- Modulo 3/Bloco 26/Dia_26.1-Intro_NodeJS/index.js
+++ Modulo 3/Bloco 26/Dia_26.1-Intro_NodeJS/index.js
@@ -23,5 +23,5 @@
 // console.log(`Valor do dólar: ${brl}`); // Valor do dólar: 5.37
 // console.log(`10 dólares em reais: ${usdToBrl(10)}`); // 10 dólares em reais: 53.7
 
-const fs = require('fs');
-fs.readFileSync('./meuArquivo.txt'); // não funfow
+// const fs = require('fs');
+// fs.readFileSync('./meuArquivo.txt'); // não funfow
