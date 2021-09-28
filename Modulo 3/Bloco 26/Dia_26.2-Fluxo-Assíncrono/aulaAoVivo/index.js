af61c299e1dcfe132af583a6d19c84ba31a5976d
Savio Moraes <smmsavio@hotmail.com>
2021-09-27 21:29:03 -0300

    aula ao vivo

---
+++ Modulo 3/Bloco 26/Dia_26.2-Fluxo-Assíncrono/aulaAoVivo/index.js
@@ -0,0 +1,43 @@
+// callbacks
+// const fs = require('fs');
+
+// const dados = fs.readFile('./teste.txt', 'utf8', (erro, conteudo) => {
+//   if(erro) return console.log(`Deu o erro': ${erro.code}`);
+//   console.log(conteudo);
+// });
+
+// utilizando callback de forma dinâmica
+// const callback = (erro, conteudo) => {
+//     if(erro) return console.log(`Deu o erro': ${erro.code}`);
+//     console.log(conteudo);
+// }
+
+// const dados = fs.readFile('./teste.txt', 'utf8', callback);
+// const dados1 = fs.readFile('./teste1.txt', 'utf8', callback);
+// const dados2 = fs.readFile('./teste2.txt', 'utf8', callback);
+
+// Promises usando a lib 'util'
+// const fs = require('fs');
+// const util = require('util');
+
+// const readFilePromise = util.promisify(fs.readFile);
+
+// readFilePromise('./teste.txt').then((response) => {
+//   console.log(response.toString());
+// }).catch(e => {
+//   console.log(e.message)
+// });
+
+// Como pegar o erro em função sincrona com o 'try' 'catch'
+// function soma() {
+//   // return 3 + 5
+//   throw new Error('Erro encontrado');
+// }
+
+// try {
+//   console.log(soma());
+// } catch (error) {
+//   console.log(error.message);
+// }
+
+/* Padrão: function sincrona try/catch, function assincrona then/catch */
