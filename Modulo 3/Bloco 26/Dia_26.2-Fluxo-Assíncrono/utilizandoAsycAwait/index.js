7969ff44998ac7e20dd76589bb37e16a9230ecbc
Savio Moraes <smmsavio@hotmail.com>
2021-10-02 15:29:35 -0300

    finaliza revisão de fluxo assíncrono

--- Modulo 3/Bloco 26/Dia_26.2-Fluxo-Assíncrono/utilizandoAsycAwait/index.js
+++ Modulo 3/Bloco 26/Dia_26.2-Fluxo-Assíncrono/utilizandoAsycAwait/index.js
@@ -2,7 +2,7 @@
 
 // async function main() {
 //   try {
-//     await fs.writeFile('', 'Modificado com async await');
+//     await fs.writeFile('./meu-arquivo.txt', 'Modificado com async await');
 //     console.log('Arquivo escrito com sucesso!');
 //   } catch (err) {
 //     console.error(`Erro ao escrever o arquivo: ${err.message}`);
@@ -13,14 +13,14 @@
 
 // sobrescreve o que está escrito no arquivo, caso exista
 // cria um novo arquivo, caso não exista
-// para gerar o erro pode-se passar o primeiro parâmetro do 'fs.writeFile como uma string vazia, aí ele não identica caminho, nome e formato e dá error!
+// para gerar o erro pode-se passar o primeiro parâmetro do 'fs.writeFile como uma string vazia, aí ele não identica caminho, nome e formato e dá error: await fs.writeFile('', 'Modificado com async await');
 
 /* Utilizando Flags no parâmetro */
 
 const fs = require('fs').promises;
 
 // A flag wx abre o arquivo para escrita **apenas** caso ele não exista. Caso o contrário, um erro será lançado, existem outras - pesquisar!
-fs.writeFile('./meu-arquivo-teste.txt', 'Eu estive aqui :eyes:', { flag: 'wx' })
+fs.writeFile('./meu-arquivo-teste1.txt', 'Eu estive aqui :eyes:', { flag: 'wx' })
   .then(() => {
     console.log('Arquivo salvo');
   })
--- Modulo 3/Bloco 26/Dia_26.2-Fluxo-Assíncrono/utilizandoAsycAwait/rodandoTudoJunto.js
+++ Modulo 3/Bloco 26/Dia_26.2-Fluxo-Assíncrono/utilizandoAsycAwait/rodandoTudoJunto.js
@@ -1,5 +1,20 @@
 const fs = require('fs').promises;
 
+// testando a falha no arquivi file2
+// Promise.all([
+//   fs.readFile('file1.txt'),
+//   fs.readFile('fail2.txt'),
+//   fs.readFile('file3.txt'),
+// ])
+//   .then(([file1, file2, file3]) => {
+//     const fileSizeSum = file1.byteLength + file2.byteLength + file3.byteLength;
+//     console.log(`Lidos 3 arquivos totalizando ${fileSizeSum} bytes`);
+//   })
+//   .catch((err) => {
+//     console.error(`Erro ao ler arquivos: ${err.message}`);
+//   });
+
+// testando sucesso!!!
 Promise.all([
   fs.readFile('file1.txt'),
   fs.readFile('file2.txt'),
