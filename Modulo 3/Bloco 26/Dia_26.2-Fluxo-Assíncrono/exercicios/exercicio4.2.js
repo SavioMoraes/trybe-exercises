5d45ddb0d8462173cd752f3cbb96aaa2748cec2f
Savio Moraes <smmsavio@hotmail.com>
2021-10-02 17:45:41 -0300

    enfim exercicio 4.2

--- Modulo 3/Bloco 26/Dia_26.2-Fluxo-Assíncrono/exercicios/exercicio4.2.js
+++ Modulo 3/Bloco 26/Dia_26.2-Fluxo-Assíncrono/exercicios/exercicio4.2.js
@@ -1,50 +1,36 @@
-// const fs = require('fs').promises;
-
-// async function getSimpsonById(id) {
-//   const simpsons = await fs
-//     .readFile('./simpsons.json', 'utf-8')
-//     .then((fileContent) => JSON.parse(fileContent));
-
-//   const chosenSimpson = simpsons.find((simpson) => simpson.id === id);
-
-//   if (!chosenSimpson) {
-//     /* A palavra-chave `throw` dispara um erro que deve ser tratado por quem chamou nossa função.
-//       * Em funções `async`, utilizar `throw` faz com que a Promise seja rejeitada,
-//       * tendo como motivo o que passarmos para o `throw`.
-//       * Ou seja, a linha abaixo rejeita a Promise da nossa função com o motivo 'id não encontrado'
-//       */
-//     throw new Error('id não encontrado');
-//   }
-
-//   /* Da mesma forma que `throw` aciona o fluxo de erro e rejeita a Promise,
-//       * `return` aciona o fluxo de sucesso e resolve a Promise.
-//       * Sendo assim, a linha abaixo é equivalente a chamar `resolve(chosenSimpson)`
-//       * dentro do executor de uma Promise.
-//       */
-//   return chosenSimpson;
-// }
-
-// getSimpsonById('1');
-/* Solucao Rodrigo Ruan */
 const fs = require('fs').promises;
 
-function showAllCharacters() {
-  return fs.readFile('simpsons.json', 'utf8')
-    .then((response) => {
-      const parsedArray = JSON.parse(response);
-      parsedArray.forEach(({ id, name }) => console.log(`${id} - ${name}`));
-    })
-    .catch((err) => console.log(`Erro ao ler o arquivo: ${err}`));
-}
-
-function findCharacterById(id) {
-  return new Promise((resolve, reject) => {
-    showAllCharacters().then((r) => {
-      const res = r.find((char) => Number(char.id) === id);
-      if (res.length === 0) reject("id não encontrado");
-      resolve(res);
-    });
+fs.readFile('./simpsons.json', 'utf-8')
+  .then((fileContent) => {
+    return JSON.parse(fileContent);
+  }).then((simpsons) => {
+    return simpsons.map(({ id, name }) => `${id} - ${name}`);
+  }).then((strings) => {
+    strings.forEach((string) => string);
   });
+
+async function getSimpsonById(id) {
+  const simpsons = await fs
+    .readFile('./simpsons.json', 'utf-8')
+    .then((fileContent) => JSON.parse(fileContent));
+
+  const chosenSimpson = simpsons.find((simpson) => simpson.id === id);
+
+  if (!chosenSimpson) {
+    /* A palavra-chave `throw` dispara um erro que deve ser tratado por quem chamou nossa função.
+      * Em funções `async`, utilizar `throw` faz com que a Promise seja rejeitada,
+      * tendo como motivo o que passarmos para o `throw`.
+      * Ou seja, a linha abaixo rejeita a Promise da nossa função com o motivo 'id não encontrado'
+      */
+    throw new Error('id não encontrado');
+  }
+
+//   /* Da mesma forma que `throw` aciona o fluxo de erro e rejeita a Promise,
+//    * `return` aciona o fluxo de sucesso e resolve a Promise.
+//    * Sendo assim, a linha abaixo é equivalente a chamar `resolve(chosenSimpson)`
+//    * dentro do executor de uma Promise.
+//    */
+  return console.log(chosenSimpson);
 }
 
-findCharacterById(2).then((r) => console.log(r));
+getSimpsonById('3');
