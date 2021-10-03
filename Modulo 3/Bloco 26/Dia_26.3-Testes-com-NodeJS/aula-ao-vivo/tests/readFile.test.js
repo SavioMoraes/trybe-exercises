1b8520662ef1ee8d0d0c40913d74ea79deb86111
Savio Moraes <smmsavio@hotmail.com>
2021-10-03 14:22:31 -0300

    aula ao vivo parte 1

---
+++ Modulo 3/Bloco 26/Dia_26.3-Testes-com-NodeJS/aula-ao-vivo/tests/readFile.test.js
@@ -0,0 +1,27 @@
+const { expect } = require('chai')
+const { readFile } = require('../readFile')
+
+describe('Chama a função "readFile"', () => {
+  describe('quando o arquivo é lido com sucesso', () => {
+    const FILE_CONTENT = 'Turma 11 é 10'
+
+    describe('a resposta', () => {
+      it('é uma string', async () => {
+        const resposta = await readFile('teste.txt')
+        expect(resposta).to.be.a('string')
+      })
+      it('é igual ao conteúdo do arquivo', async () => {
+        const resposta = await readFile('teste.txt')
+        expect(resposta).to.be. equal(FILE_CONTENT)
+      })
+    })
+  })
+  describe('quando ocorrer um erro na leitura do arquivo', () => {
+    describe('a resposta', () => {
+      it('é igual a "null"', async () => {
+        const resposta = await readFile('testeNull.txt')
+        expect(resposta).to.be.null
+      })
+    })
+  })
+})
