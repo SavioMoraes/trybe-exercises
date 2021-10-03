eba42e0630bdb2d66b5a96b3679c38ba2bed2c86
Savio Moraes <smmsavio@hotmail.com>
2021-10-03 10:57:48 -0300

    conteudo course testes nodeJS parte 1 nao funfando calculadora.js

--- Modulo 3/Bloco 26/Dia_26.3-Testes-com-NodeJS/examples/calculaSituacao.js
+++ Modulo 3/Bloco 26/Dia_26.3-Testes-com-NodeJS/examples/calculaSituacao.js
@@ -1,9 +1,11 @@
 function calculaSituacao(media) {
-  if (media >= 7) {
+  if (media > 7) {
     return 'aprovado';
   }
 
   return 'reprovado';
 }
+
+// gerado erro de propósito para o teste manual em index.js falhar, quando igual a 7! Para corrigir basta passar o operador de comparação >= no lugar do >.
 
 module.exports = calculaSituacao;
---
+++ Modulo 3/Bloco 26/Dia_26.3-Testes-com-NodeJS/examples/soma.js
@@ -0,0 +1,5 @@
+// Aqui podemos escrever testes pensando somente o comportamento esperado para função `soma`
+
+const soma = (valorA, valorB) => valorA + valorB;
+
+module.exports = soma;
