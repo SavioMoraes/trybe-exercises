68220aa836b95744ffb4becb13537124130967c3
Savio Moraes <smmsavio@hotmail.com>
2021-10-15 15:25:40 -0300

    bloco-26_novo.2 até validações

--- Modulo 3/Bloco 26_novo/Dia_2-ArquiteturaDeSoftware_Camadas_Controller_e_Service/msc/index.js
+++ Modulo 3/Bloco 26_novo/Dia_2-ArquiteturaDeSoftware_Camadas_Controller_e_Service/msc/index.js
@@ -1,78 +1,28 @@
+// index.js
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const app = express();
+const Author = require('./controllers/Author');
+const Book = require('./controllers/Book');
+const errorMiddleware = require('./middlewares/error');
 
-const PORT = process.env.PORT || 3000;
+const app = express();
 
 app.use(bodyParser.json());
 
-const Author = require('./services/Author');
-const Book = require('./models/Book');
+app.get('/authors', Author.getAll);
+app.get('/authors/:id', Author.findById);
+app.post('/authors', Author.create);
 
-// rota authors
-app.get('/authors', async (req, res) => {
-  const authors = await Author.getAll();
-  
-  res.status(200).json(authors);
-});
+app.get('/books', Book.getAll);
+app.get('/books/:id', Book.findById);
+app.post('/books', Book.create);
 
-app.get('/authors/:id', async (req, res) => {
-  const { id } = req.params;
+app.use(errorMiddleware);
 
-  const author = await Author.findById(id);
-
-  if (!author) return res.status(404).json({ message: 'Not Found'});
-
-  res.status(200).json(author);
-});
-
-app.post('/authors', async (req, res) => {
-  const { first_name, middle_name, last_name } = req.body;
-
-  const author = await Author.create(first_name, middle_name, last_name);
-  
-  if (!author) return res.status(400).json({ message: 'Dados inválidos'});
-
-  res.status(201).json(author)
-});
-
-// rota books
-// utilize este código no Postman para utilizar o endoint abaixo...
-// localhost:3000/books?author_id=1 
-app.get('/books', async (req, res) => {
-  const { author_id } = req.query;
-
-  const books = (author_id)
-  ? await Book.getByAuthorId(author_id)
-  : await Book.getAll();
-
-  res.status(200).json(books);
-});
-
-app.get('/books/:_id', async (req, res) => {
-  const { _id } = req.params;
-
-  // const book = await Book.getById(id); // MYSQL: funfandooo!!!
-  const book = await Book.findById(_id); // MongoDB: funfandooooo!!!!! Tem que passar _id ao invés de id! na rota e nos parâmetros! Além de ter que chamar o ObjectId do mongo em Book.js para poder pegar o _id do objeto do mongo!
-
-  if (!book) return res.status(404).json({ message: 'Book not found' })
-
-  res.status(200).json(book);
-});
-
-app.post('/books', async (req, res) => {
-  const { title, author_id } = req.body;
-
-  if (!await Book.isValid(title, author_id)) {
-    return res.status(400).json({ message: 'Dados inválidos' });
-  }
-
-  await Book.create(title, author_id);
-
-  res.status(201).json({ message: 'Livro criado com sucesso! '});
-});
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Ouvindo a porta ${PORT}`);
+  console.log(`Ouvindo a porta ${PORT}`);
 });
--- Modulo 3/Bloco 26_novo/Dia_2-ArquiteturaDeSoftware_Camadas_Controller_e_Service/msc/models/Book.js
+++ Modulo 3/Bloco 26_novo/Dia_2-ArquiteturaDeSoftware_Camadas_Controller_e_Service/msc/models/Book.js
@@ -1,65 +1,6 @@
 const connection = require('./connection');
 const Author = require('./Author');
 
-/* MYSQL */
-// const getAll = async () => {
-//   const [books] = await connection.execute('SELECT * FROM model_example.books;');
-
-//   return books.map(({id, title, author_id}) => ({
-//     id,
-//     title,
-//     authorId: author_id,
-//   }));
-// };
-
-
-/* MYSQL */
-// const getByAuthorId = async (authorId) => {
-//   const query = 'SELECT * FROM model_example.books WHERE author_id=?;'
-//   const [books] = await connection.execute(query, [authorId]);
-
-//   return books.map(({ id, title, author_id }) => ({
-//     id,
-//     title,
-//     authorId: author_id,
-//   }));
-// };
-
-/* MYSQL */
-// const getById = async (id) => {
-//   const query = 'SELECT * FROM model_example.books WHERE id=?;'
-
-//   const [books] = await connection.execute(query, [id]);
-
-//   if (books.length === 0) return null;
-
-//   return books.map(({ id, title, author_id }) => ({
-//     id,
-//     title,
-//     authorId: author_id,
-//   }))[0];
-// };
-
-
-/* MYSQL */
-// const isValid = async (title, authorId) => {
-//   if (!title || typeof title !== 'string' || title.length < 3) return false;
-//   if (!authorId || typeof authorId !== 'number' || !(await Author.findById(authorId))) return false;
-
-//   return true;
-// };
-
-
-/* MYSQL */
-// const create = async (title, authorId) => connection.execute(
-// 'INSERT INTO model_example.books (title, author_id) VALUES (?,?)',
-// [title, authorId],
-// );
-
-//----------------------------------------------------------------------------
-
-// Utilizando MongoDB <<<---
-
 const { ObjectId } = require('mongodb');
 
 const getAll = () => connection()
