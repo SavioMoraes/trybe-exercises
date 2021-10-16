c0ec7f9d33d8314a8b86d44a3772772b3342f584
Savio Moraes <smmsavio@hotmail.com>
2021-10-16 02:25:30 -0300

    inicia resolução do exercicio do bloco 26.3

--- Modulo 3/Bloco 26_novo/Dia_3-ArquiteturaWeb_Rest_e_Restful/exercicios_26.3/controllers/productController.js
+++ Modulo 3/Bloco 26_novo/Dia_3-ArquiteturaWeb_Rest_e_Restful/exercicios_26.3/controllers/productController.js
@@ -1,40 +1,55 @@
 const express = require('express');
-const ProductModel = require('../models/productModel');
-
+const ProductModel = require("../models/productModel");
 const router = express.Router();
 
-router.get('/list-products', async (req, res, next) => {
+router.get('/', async (req, res, next) => {
   const products = await ProductModel.getAll();
 
-  res.send(products);
+  res.status(200).json(products);
 });
 
-router.get('/get-by-id/:id', async (req, res, next) => {
+router.get('/:id', async (req, res, next) => {
   const product = await ProductModel.getById(req.params.id);
 
-  res.send(product);
+  if (product === null) {
+    res.status(404).send({ message: 'Produto não encontrado' });
+  }
+
+  res.status(200).json(product);
 });
 
-router.post('/add-user', async (req, res) => {
+router.post('/', async (req, res) => {
   const { name, brand } = req.body;
 
-  const newProduct = await ProductModel.add(name, brand);
+  try {
+    const newProduct = await ProductModel.add(name, brand);
 
-  res.send(newProduct);
+    res.status(200).json(newProduct);
+  } catch (e) {
+    res.status(500).send({ message: 'Algo deu errado' });
+  }
 });
 
-router.post('/delete-user/:id', async (req, res) => {
-  const products = await ProductModel.exclude(req.params.id);
+router.delete('/:id', async (req, res) => {
+  try {
+    const products = await ProductModel.exclude(req.params.id);
 
-  res.send(products);
+    res.status(200).json(products);
+  } catch (e) {
+    res.status(500).send({ message: 'Algo deu errado' });
+  }
 });
 
-router.post('/update-user/:id', async (req, res) => {
+router.put('/:id', async (req, res) => {
   const { name, brand } = req.body;
 
-  const products = await ProductModel.update(req.params.id, name, brand);
+  try {
+    const products = await ProductModel.update(req.params.id, name, brand);
 
-  res.send(products);
+    res.status(200).json(products);
+  } catch (e) {
+    res.status(500).send({ message: 'Algo deu errado' });
+  }
 });
 
 module.exports = router;
