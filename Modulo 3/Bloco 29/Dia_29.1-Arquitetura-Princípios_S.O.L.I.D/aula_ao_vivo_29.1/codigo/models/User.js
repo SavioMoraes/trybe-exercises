28641394ab5ca886c3bdab9c444e8c76f41a2a4d
Savio Moraes <smmsavio@hotmail.com>
2021-11-25 20:36:30 -0300

    conteudo courso sequelize do zero, cria app e instala express e nodemon alem de setar o start para o nodemon

--- Modulo 3/Bloco 29/Dia_29.1-Arquitetura-Princípios_S.O.L.I.D/aula_ao_vivo_29.1/codigo/models/User.js
+++ Modulo 3/Bloco 29/Dia_29.1-Arquitetura-Princípios_S.O.L.I.D/aula_ao_vivo_29.1/codigo/models/User.js
@@ -2,7 +2,7 @@ const connectionFactory = require("./connectionFactory");
 const schema = require("./schemas");
 
 const create = async (username, email, password, role) => {
-  const connection = schema.connection2;
+  const connection = schema.connection1;
   connectionFactory(connection).execute(
     "INSERT INTO users (username, email, password, role) VALUES (?,?,?,?)",
     [username, email, password, role]
