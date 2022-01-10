e36687a63143af4a983ba4473a9683d51f57bc40
Savio Moraes <smmsavio@hotmail.com>
2022-01-10 16:42:28 -0300

    cria controllers/authorController.js e index.js na raiz do projeto

---
+++ Modulo 3/Bloco 30/Dia_1-Arquitetura_de_Software-Camada_de_View/mvc-example/controllers/authorController.js
@@ -0,0 +1,9 @@
+const Author = require('../models/Author');
+
+const listAuthors = async (req, res) => {
+ // Já vamos voltar para adicionar a lógica aqui
+};
+
+module.exports = {
+  listAuthors
+}
