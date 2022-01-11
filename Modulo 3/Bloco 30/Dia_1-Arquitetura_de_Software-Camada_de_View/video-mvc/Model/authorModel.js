bf05c2e053c36a4eb25e4638ada2a74799f472e4
Savio Moraes <smmsavio@hotmail.com>
2022-01-11 11:16:54 -0300

    em authorModel, serializa os dados para camelCase e inclui a coluna name recebendo fullName

--- Modulo 3/Bloco 30/Dia_1-Arquitetura_de_Software-Camada_de_View/video-mvc/Model/authorModel.js
+++ Modulo 3/Bloco 30/Dia_1-Arquitetura_de_Software-Camada_de_View/video-mvc/Model/authorModel.js
@@ -1,9 +1,33 @@
 const connection = require('./connection');
 
-const findAllAuthors = async () => {
-  const [authors] = await connection.execute('SELECT * FROM authors');
+// Serializa o nome dos campos de snake_case para camelCase
+const serialize = (authorData) => ({
+  id: authorData.id,
+  firstName: authorData.first_name,
+  middleName: authorData.middle_name,
+  lastName: authorData.last_name,
+});
 
-  return authors;
+const getNewAuthor = (authorData) => {
+  const { id, firstName, middleName, lastName } = authorData;
+
+  const fullName = [firstName, middleName, lastName]
+    .filter((name) => name)
+    .join(' ');
+
+  return {
+    id,
+    firstName,
+    middleName,
+    lastName,
+    name: fullName,
+  };
+};
+
+const findAllAuthors = async () => {
+  const [authors] = await connection.execute('SELECT id, first_name, middle_name, last_name FROM authors');
+
+  return authors.map(serialize).map(getNewAuthor);
 };
 
 module.exports = {
