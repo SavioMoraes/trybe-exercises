7b49cd320599aa229f1fbc23f2deb351aa92deab
Savio Moraes <smmsavio@hotmail.com>
2022-01-11 11:22:13 -0300

    altera retorno ara visualizaçao por json no Postman

--- Modulo 3/Bloco 30/Dia_1-Arquitetura_de_Software-Camada_de_View/video-mvc/Controller/authorController.js
+++ Modulo 3/Bloco 30/Dia_1-Arquitetura_de_Software-Camada_de_View/video-mvc/Controller/authorController.js
@@ -3,7 +3,8 @@ const authorModel = require('../Model/authorModel');
 const listAuthors = async (req, res) => {
   const authors = await authorModel.findAllAuthors();
 
-  res.status(200).render('Author/Author', { authors });
+  // res.status(200).render('Author/Author', { authors }); // .render para visualizar na camada view (html feito com ejs!)
+  res.status(200).json(authors); // retornando formato json para visualizar no Postman
 };
 
 module.exports = {
