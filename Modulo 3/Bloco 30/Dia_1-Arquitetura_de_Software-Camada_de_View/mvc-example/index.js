39a9f2f923fad8bf8f051e386c2f21ab3e083a26
Savio Moraes <smmsavio@hotmail.com>
2022-01-10 21:15:37 -0300

    add find by id e cria views show.ejs e 404.ejs

--- Modulo 3/Bloco 30/Dia_1-Arquitetura_de_Software-Camada_de_View/mvc-example/index.js
+++ Modulo 3/Bloco 30/Dia_1-Arquitetura_de_Software-Camada_de_View/mvc-example/index.js
@@ -9,6 +9,7 @@ app.set('view engine', 'ejs');
 app.set('views', './views');
 
 app.get('/authors', authorController.listAuthors);
+app.get('/authors/:id', authorController.showAuthor);
 
 const PORT = process.env.PORT || 3000;
 
