545e6b135eed7460ec7d8d20229fa145fa67271c
Savio Moraes <smmsavio@hotmail.com>
2022-01-10 23:29:42 -0300

    configura ejs em index.js

--- Modulo 3/Bloco 30/Dia_1-Arquitetura_de_Software-Camada_de_View/video-mvc/index.js
+++ Modulo 3/Bloco 30/Dia_1-Arquitetura_de_Software-Camada_de_View/video-mvc/index.js
@@ -5,6 +5,9 @@ const authorController = require('./Controller/authorController');
 const app = express();
 const port = 3000;
 
+app.set('view engine', 'ejs');
+app.set('views', './view')
+
 app.get('/authors', authorController.listAuthors);
 
 app.listen(port, () => console.log(`\nUP ON PORT: ${port}!`));
