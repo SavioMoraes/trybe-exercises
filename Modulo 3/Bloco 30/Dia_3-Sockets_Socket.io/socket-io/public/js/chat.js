1ab406720964eda6995be511ee7538e6e909cfb5
Savio Moraes <smmsavio@hotmail.com>
2022-01-13 00:24:05 -0300

    cria chat cliente e servidor

---
+++ Modulo 3/Bloco 30/Dia_3-Sockets_Socket.io/socket-io/public/js/chat.js
@@ -0,0 +1,21 @@
+const socket = window.io();
+
+const form = document.querySelector('form');
+const inputMessage = document.querySelector('#messageInput');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  socket.emit('clientMessage', inputMessage.value);
+  inputMessage.value = '';
+  return false;
+
+});
+
+const createMessage = (message) => {
+  const messageUl = document.querySelector('#messages');
+  const li = document.createElement('li');
+  li.innerText = message;
+  messageUl.appendChild(li);
+}
+
+socket.on('serverMessage', (message) => createMessage(message));
---
+++ Modulo 3/Bloco 30/Dia_3-Sockets_Socket.io/socket-io/sockets/chat.js
@@ -0,0 +1,6 @@
+module.exports = (io) => io.on('connection', (socket) => {
+  socket.on('clientMessage', (message) => {
+    console.log(`Mensagem ${message}`);
+    io.emit('serverMessage', message);
+  });
+});
