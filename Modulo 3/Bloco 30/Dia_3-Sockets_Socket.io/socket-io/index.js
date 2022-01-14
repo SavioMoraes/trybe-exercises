6d0939b62b39c0d24786b60ab2f6e4167bfc8c25
Savio Moraes <smmsavio@hotmail.com>
2022-01-14 00:44:43 -0300

    muda endereço da porta por causa de conflito

--- Modulo 3/Bloco 30/Dia_3-Sockets_Socket.io/socket-io/index.js
+++ Modulo 3/Bloco 30/Dia_3-Sockets_Socket.io/socket-io/index.js
@@ -4,7 +4,7 @@ const http = require('http').createServer(app);
 
 const io = require('socket.io')(http, {
   cors: {
-    origin: 'http://localhost:3000', // url aceita pelo cors
+    origin: 'http://localhost:3001', // url aceita pelo cors
     methods: ['GET', 'POST'], // Métodos aceitos pela url
 }});
 
@@ -18,6 +18,6 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(3000, () => {
-  console.log('Servidor ouvindo na porta 3000');
+http.listen(3001, () => {
+  console.log('Servidor ouvindo na porta 3001');
 });
