efa74e76df21236fa6c4bff7807bfa28dc357f3f
Savio Moraes <smmsavio@hotmail.com>
2022-02-08 22:49:26 -0300

    exercicios course

--- Modulo 4/Bloco_32-Introdução_à_Python/Dia_01/operacoes_basicas/tempCodeRunnerFile.py
+++ Modulo 4/Bloco_32-Introdução_à_Python/Dia_01/operacoes_basicas/tempCodeRunnerFile.py
@@ -1,2 +1,6 @@
-print(3 // 2)
-print(3 / 2)
+ratings = [6, 8, 5, 9, 10]
+
+for rating in ratings:
+    # o sinal % representa a operação "resto".
+    if (rating % 3) == 0: # se o resto é zero, é divisível
+        print(f'{rating} é múltiplo de 3')
