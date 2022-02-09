bcbe9be2409ca71b9f9dd1074f5936b449adeccc
Savio Moraes <smmsavio@hotmail.com>
2022-02-08 21:44:55 -0300

    operaçoes basicas

---
+++ Modulo 4/Bloco_32-Introdução_à_Python/Dia_01/operacoes_basicas/basic_operations.py
@@ -0,0 +1,25 @@
+print(2 * 3)  # saída: 6
+print(2 + 3)  # saída: 5
+print(3 / 2)  # saída: 1.5
+
+square_root = 25 ** (1/2)
+# raiz quadrada de 25.
+# O operador `**` significa "elevado a"
+print(square_root + 1)  # saída: 6.0
+
+# counter = 0
+# counter++  # esse código vai falhar
+
+# original
+counter = 0
+counter = counter + 1
+print(counter)
+
+# simplificado
+count = 0
+count += 1
+print(count)
+print(count)
+
+print(3 // 2)
+print(3 / 2)
