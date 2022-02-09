cca9c3c7c7fbc16ee4baa97c5f3cc339e0567b39
Savio Moraes <smmsavio@hotmail.com>
2022-02-09 12:37:14 -0300

    dicas "help"

---
+++ Modulo 4/Bloco_32-Introdução_à_Python/Dia_01/uma_úlitima_dica/dica.py
@@ -0,0 +1,27 @@
+import math  # uilizado nos exemplos a partir da liha 26...
+
+# Você deve estar pensando agora: "Socorro! Acho que não vou conseguir
+# me lembrar de tudo o que vi, posso acabar me confundindo com outras
+# linguagens.".
+
+# Então vou te dar um conselho! Sempre que estiver programando e surgir
+# alguma dúvida, abra o terminal interativo e verifique o que está fazendo.
+# Ele pode ser seu aliado, e pode inclusive te dar uma ajuda quando for
+# necessário.
+
+# Temos um comando que chama help , e ele pode ser utilizado até mesmo
+# em cláusulas if ou for , desde que colocado entre aspas.
+
+help("if")  # no terminal do python
+
+# A tecla q deve ser apertada para sair da ajuda.
+
+
+# importando de módulos embutidos:
+
+
+help(abs)
+
+help(len)
+
+help(math.sin)
---
+++ Modulo 4/Bloco_32-Introdução_à_Python/Dia_01/uma_úlitima_dica/tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+import math
+
+print(help(abs))
+
+print(help(len))
+
+print(help(math.sin))
