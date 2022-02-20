c1a07a9dbb99ede49c6518356f6fb0da742793d2
Savio Moraes <smmsavio@hotmail.com>
2022-02-20 18:56:25 -0300

    exercicio 03

--- Modulo 4/Bloco_32-Introdução_à_Python/Dia_03/exercicios/test_exercicios.py
+++ Modulo 4/Bloco_32-Introdução_à_Python/Dia_03/exercicios/test_exercicios.py
@@ -1,5 +1,5 @@
 import pytest
-from exercicios import fizzbuzz, translate_to_number
+from exercicios import fizzbuzz, translate_to_number, validate_email
 
 
 def test_fizzbuzz_should_return_list_of_numbers():
@@ -73,3 +73,51 @@ def test_expression_maximum_are_thirty_chars():
 def test_expression_with_invalid_chars():
     with pytest.raises(ValueError):
         translate_to_number("I-****-MY_JOB")
+
+
+def test_username_can_only_contain_letters():
+    assert validate_email("aaaa@nomewebsite.ext") is None
+
+
+def test_username_can_contain_letters_and_digits():
+    assert validate_email("a1993@nomewebsite.ext") is None
+
+
+def test_username_can_contain_letters_digits_and_dashes():
+    assert validate_email("aa-a@nomewebsite.ext") is None
+
+
+def test_username_can_contain_letters_digits_dashes_and_underscores():
+    assert validate_email("a_a1a-a@nomewebsite.ext") is None
+
+
+def test_username_should_starts_with_letter():
+    assert validate_email("a@nomewebsite.ext") is None
+
+
+def test_username_when_dont_start_with_letter_raise_exception():
+    with pytest.raises(ValueError):
+        validate_email("1@nomewebsite.ext")
+
+
+def test_username_is_invalid_raise_exception():
+    with pytest.raises(ValueError):
+        validate_email("a%a@nomewebsite.ext")
+
+
+def test_website_contain_only_letters_and_digits():
+    assert validate_email("abc@website123.com") is None
+
+
+def test_website_invalid_chars_raise_exception():
+    with pytest.raises(ValueError):
+        validate_email("abc@website!123.com")
+
+
+def test_extension_should_contain_only_three_chars():
+    assert validate_email("abc@website123.com") is None
+
+
+def test_extension_invalid_chars_raise_exception():
+    with pytest.raises(ValueError):
+        validate_email("abc@website123.pyth")
