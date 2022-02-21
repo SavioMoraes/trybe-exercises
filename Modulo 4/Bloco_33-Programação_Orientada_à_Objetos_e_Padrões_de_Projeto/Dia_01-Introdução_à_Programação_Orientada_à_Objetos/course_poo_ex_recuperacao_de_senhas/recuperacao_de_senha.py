ba0c63a60f5d69a6ec664fcce99369faf6a54a23
Savio Moraes <smmsavio@hotmail.com>
2022-02-21 13:33:37 -0300

    Abstraçao e Encapsulamento

--- Modulo 4/Bloco_33-Programação_Orientada_à_Objetos_e_Padrões_de_Projeto/Dia_01-Introdução_à_Programação_Orientada_à_Objetos/course_poo_ex_recuperacao_de_senhas/recuperacao_de_senha.py
+++ Modulo 4/Bloco_33-Programação_Orientada_à_Objetos_e_Padrões_de_Projeto/Dia_01-Introdução_à_Programação_Orientada_à_Objetos/course_poo_ex_recuperacao_de_senhas/recuperacao_de_senha.py
@@ -1,33 +1,46 @@
 class User:  # entidade geral - definimos uma Classe
     def __init__(self, name, email, password):
-        # __init__, trata-se do método Constructor, que tem que estar
-        # presente nas classes capazez de criar Objetos!
         """ Método construtor da classe User. Note que
         o primeiro parâmetro deve ser o `self`. Isso é
         uma particularidade de Python, vamos falar mais
         disso adiante!"""
-        self.name = name
-        self.email = email
-        self.password = password
+        # __init__, trata-se  da sintaxe do método Constructor,
+        # que tem que estar presente nas classes capazez de
+        # criar Objetos!
+        # esse método possui atributos (informações acerca dessa entidade):
+        self.name = name  # atributo name
+        self.email = email  # atributo email
+        self.password = password  # atributo password
 
-    def reset_password(self):
+    def reset_password(self):  # isso é um método em POO
         print("Envia email de reset de senha")
+
+    def diga_seu_nome(self):
+        return self.name
+
+# Métodos são as coisas que a entidade faz...
+# Atributos são as coisas que a entidade sabe...
 
 
 # Para invocar o método construtor, a sintaxe é:
 # NomeDaClasse(parametro 1, parametro 2)
 # Repare que o parâmetro self foi pulado -- um detalhe do Python.
-meu_user = User("Valentino Trocatapa", "valentino@tinytoons.com", "Grana")
+valentino = User("Valentino Trocatapa", "valentino@tinytoons.com", "Grana")
+lilica = User("Lilica", "lilica@tinytoons.com", "coelho")
+perninha = User("Perninha", "perninha@tinytoons.com", "coelho123")
 # entidade específica para nosso usuário
 # criamos um Objeto, a partir da Classe definida!
 # A variável `meu_user` contém o objeto criado pelo construtor da
 # classe User!
-print(meu_user)
-print(meu_user.name)
-print(meu_user.email)
-print(meu_user.password)
+print(lilica)  # contém um objeto do tipo User!
+print(lilica.name)
+print(lilica.email)
+print(lilica.password)
 
-meu_user.reset_password()
+retorno_do_metodo_diga_seu_nome = perninha.diga_seu_nome()
+print(retorno_do_metodo_diga_seu_nome)
+
+valentino.reset_password()
 # Se definimos numa classe uma função, podemos chamar ela
 # a partir do objeto que criamos!
 
@@ -44,3 +57,9 @@ meu_user.reset_password()
 # # # Já sei o suficiente pra agir!
 # # meu_user = User("Valentino Trocatapa", "valentino@tinytoons.com", "Grana")
 # # meu_user.reset_password()
+
+# CRIE ENTIDADES = Abstração!!!
+# NÃO PRECISO SABER COMO FUNCIONA = Encapsulamento!!!
+
+# ABSTRAÇÃO - 1º Pilar da Programação Orientada à Objetos
+# ENCAPSULAMENTO - 2º Pilar da POO
