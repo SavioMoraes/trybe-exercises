093dbcfaa322883d638be766e48e1df30ce8cff2
Savio Moraes <smmsavio@hotmail.com>
2022-02-21 11:09:15 -0300

    insere o constructor na definiçao da classe e cria um objeto a partir dela

--- Modulo 4/Bloco_33-Programação_Orientada_à_Objetos_e_Padrões_de_Projeto/Dia_01-Introdução_à_Programação_Orientada_à_Objetos/course_poo_ex_recuperacao_de_senhas/recuperacao_de_senha.py
+++ Modulo 4/Bloco_33-Programação_Orientada_à_Objetos_e_Padrões_de_Projeto/Dia_01-Introdução_à_Programação_Orientada_à_Objetos/course_poo_ex_recuperacao_de_senhas/recuperacao_de_senha.py
@@ -1,11 +1,24 @@
-class User:
+class User:  # entidade geral - definimos uma Classe
     def __init__(self, name, email, password):
+        # __init__, trata-se do método Constructor, que tem que estar
+        # presente nas classes capazez de criar Objetos!
+        """ Método construtor da classe User. Note que
+        o primeiro parâmetro deve ser o `self`. Isso é
+        uma particularidade de Python, vamos falar mais
+        disso adiante!"""
         self.name = name
         self.email = email
         self.password = password
 
 
+# Para invocar o método construtor, a sintaxe é:
+# NomeDaClasse(parametro 1, parametro 2)
+# Repare que o parâmetro self foi pulado -- um detalhe do Python.
 meu_user = User("Valentino Trocatapa", "valentino@tinytoons.com", "Grana")
+# entidade específica para nosso usuário
+# criamos um Objeto, a partir da Classe definida!
+# A variável `meu_user` contém o objeto criado pelo construtor da
+# classe User!
 print(meu_user)
 print(meu_user.name)
 print(meu_user.email)
