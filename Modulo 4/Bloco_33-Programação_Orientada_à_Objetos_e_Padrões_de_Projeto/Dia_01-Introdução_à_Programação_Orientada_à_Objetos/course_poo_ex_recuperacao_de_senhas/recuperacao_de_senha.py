509f8c5153da0ce86e899eae74fe325a0566c9a0
Savio Moraes <smmsavio@hotmail.com>
2022-02-21 10:37:57 -0300

    criando uma classe User geral e deois uma entidade: meu_user

--- Modulo 4/Bloco_33-Programação_Orientada_à_Objetos_e_Padrões_de_Projeto/Dia_01-Introdução_à_Programação_Orientada_à_Objetos/course_poo_ex_recuperacao_de_senhas/recuperacao_de_senha.py
+++ Modulo 4/Bloco_33-Programação_Orientada_à_Objetos_e_Padrões_de_Projeto/Dia_01-Introdução_à_Programação_Orientada_à_Objetos/course_poo_ex_recuperacao_de_senhas/recuperacao_de_senha.py
@@ -3,3 +3,10 @@ class User:
         self.name = name
         self.email = email
         self.password = password
+
+
+meu_user = User("Valentino Trocatapa", "valentino@tinytoons.com", "Grana")
+print(meu_user)
+print(meu_user.name)
+print(meu_user.email)
+print(meu_user.password)
