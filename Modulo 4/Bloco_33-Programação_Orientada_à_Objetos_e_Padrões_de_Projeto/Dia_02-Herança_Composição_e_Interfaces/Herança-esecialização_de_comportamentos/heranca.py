eed7d99e004e882a06f282442224eb31b7e40d54
Savio Moraes <smmsavio@hotmail.com>
2022-02-21 20:50:27 -0300

    Herança - Especialização de comortamentos

---
+++ Modulo 4/Bloco_33-Programação_Orientada_à_Objetos_e_Padrões_de_Projeto/Dia_02-Herança_Composição_e_Interfaces/Herança-esecialização_de_comportamentos/heranca.py
@@ -0,0 +1,52 @@
+from abc import ABC, abstractmethod
+from csv import DictWriter
+import json
+
+
+class SalesReport(ABC):
+    def __init__(self, export_file):
+        self.export_file = export_file
+
+    def build(self):
+        return [{
+                'Coluna 1': 'Dado 1',
+                'Coluna 2': 'Dado 2',
+                'Coluna 3': 'Dado 3'
+                },
+                {
+                'Coluna 1': 'Dado A',
+                'Coluna 2': 'Dado B',
+                'Coluna 3': 'Dado C'
+                }]
+
+    @abstractmethod
+    def serialize(self):
+        raise NotImplementedError
+
+
+class SalesReportJSON(SalesReport):
+    def serialize(self):
+        with open(self.export_file + '.json', 'w') as file:
+            json.dump(self.build(), file)
+
+
+class SalesReportCSV(SalesReport):
+    def serialize(self):
+        with open(self.export_file + '.csv', 'w') as file:
+            headers = ["Coluna 1", "Coluna 2", "Coluna 3"]
+            csv_writer = DictWriter(file, headers)
+            csv_writer.writeheader()
+            for item in self.build():
+                csv_writer.writerow(item)
+
+
+# Para testar
+relatorio_de_vendas = SalesReportJSON('meu_relatorio')
+relatorio_de_vendas.serialize()
+
+relatorio_de_compras = SalesReportCSV('meu_relatorio')
+relatorio_de_compras.serialize()
+
+
+relatorio_de_vendas = SalesReportJSON('meu_relatorio')
+relatorio_de_vendas.serialize()
