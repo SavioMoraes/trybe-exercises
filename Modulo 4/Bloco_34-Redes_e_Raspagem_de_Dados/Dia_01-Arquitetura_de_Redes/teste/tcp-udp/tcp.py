8ad6ab773962281c60290c630bac222fc5e97d16
Savio Moraes <smmsavio@hotmail.com>
2022-03-01 01:41:43 -0300

    exemplos aula 34.1 T11

---
+++ Modulo 4/Bloco_34-Redes_e_Raspagem_de_Dados/Dia_01-Arquitetura_de_Redes/teste/tcp-udp/tcp.py
@@ -0,0 +1,22 @@
+from socketserver import TCPServer, StreamRequestHandler
+# socketserver, nativo do python. importando as classes TCPServer, que
+# é nosso servidor TCP e outra pra gerenciar as requisições (handle) e
+# como vai ser de arquivo (conjunto de bits - 0 e 1), utiliza-se o
+# StreamRequestHandler
+
+
+class TCPHandler(StreamRequestHandler):
+    def handle(self):
+        self.wfile.write(b"Ola, cliente\n")
+        while True:
+            data = self.rfile.readline().strip().decode("UTF-8")
+            if not data:
+                self.wfile.white(b"Cliente desconectado")
+                break
+        print(data)
+
+
+server_adrees = ("localhost", 8080)
+with TCPServer(server_adrees, TCPHandler) as server:
+    print("Server TCP ativo")
+    server.serve_forever()
