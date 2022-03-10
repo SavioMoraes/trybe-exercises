d5e10e6daad48e6e09bccebb1216f2cfd4a5f41a
Savio Moraes <smmsavio@hotmail.com>
2022-03-10 02:02:57 -0300

    Raspagem de dados: Alguns problemas

--- Modulo 4/Bloco_34-Redes_e_Raspagem_de_Dados/Dia_03-Raspagem_de_Dados/Alguns_Problemas/crawler/crawler.py
+++ Modulo 4/Bloco_34-Redes_e_Raspagem_de_Dados/Dia_03-Raspagem_de_Dados/Alguns_Problemas/crawler/crawler.py
@@ -1,10 +1,10 @@
-import requests
-import time
+# import requests
+# import time
 
-for _ in range(15):
-    response = requests.get("https://www.cloudflare.com/rate-limit-test/")
-    print(response.status_code)
-    time.sleep(6)
+# for _ in range(15):
+#     response = requests.get("https://www.cloudflare.com/rate-limit-test/")
+#     print(response.status_code)
+#     time.sleep(6)
 
 # No exemplo acima, se colocarmos o time, cada requisição será
 # efetuada pós 6 segundos (passados por parâmetro), fazendo com
@@ -12,3 +12,13 @@ for _ in range(15):
 # passado o time, acontece uma encherrada de requisições quase
 # simultâneas, o que causará o bloqueio ao acesso à página gerando
 # um erro 429 (too many request)
+
+import requests
+
+
+try:
+    response = requests.get("http://httpbin.org/delay/10", timeout=2)
+except requests.Timeout:
+    response = requests.get("http://httpbin.org/delay/1", timeout=2)
+finally:
+    print(response.status_code)
