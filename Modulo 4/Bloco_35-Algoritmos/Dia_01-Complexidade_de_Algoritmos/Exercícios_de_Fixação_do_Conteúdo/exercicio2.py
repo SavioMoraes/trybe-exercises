f19d8be874169d87316f8370d137667070de7afd
Savio Moraes <smmsavio@hotmail.com>
2022-03-18 03:28:35 -0300

    complexidade quadrática e cúbica(exercicio-content-3)

---
+++ Modulo 4/Bloco_35-Algoritmos/Dia_01-Complexidade_de_Algoritmos/Exercícios_de_Fixação_do_Conteúdo/exercicio2.py
@@ -0,0 +1,22 @@
+def multiply_arrays(array1, array2):
+    result = []
+    number_of_iterations = 0
+
+    for number1 in array1:
+        print(f'Array 1: {number1}')
+        for number2 in array2:
+            print(f'Array 2: {number2}')
+            result.append(number1 * number2)
+            number_of_iterations += 1
+
+    print(f'{number_of_iterations} iterações!')
+    return result
+
+
+# meu_array = [1, 2, 3, 4, 5]
+meu_array = list(range(1, 1000))
+
+multiply_arrays(meu_array, meu_array)
+
+# para medir o tempo (melhor maneira!) digite o comando:
+# time python3 nome_do_arquivo.py
