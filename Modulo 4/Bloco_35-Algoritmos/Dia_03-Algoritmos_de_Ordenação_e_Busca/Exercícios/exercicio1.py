cbc1bec8a7628189750110c477625cc63aaca07d
Savio Moraes <smmsavio@hotmail.com>
2022-03-19 20:56:45 -0300

    exercicios 1 e 2 do bloco 35.3

---
+++ Modulo 4/Bloco_35-Algoritmos/Dia_03-Algoritmos_de_Ordenação_e_Busca/Exercícios/exercicio1.py
@@ -0,0 +1,10 @@
+"""
+Exercício 1:
+    Dado um array com os seguintes elementos:
+    ["zebra", "macaco", "elefante", "arara", "javali"],
+    após duas iterações utilizando bubble sort,
+    como estaria este array?
+"""
+
+# Solução:
+# ["elefante", "arara", "javali", "macaco", "zebra"]
---
+++ Modulo 4/Bloco_35-Algoritmos/Dia_03-Algoritmos_de_Ordenação_e_Busca/Exercícios/exercicio2.py
@@ -0,0 +1,16 @@
+"""
+Exercício 2:
+Demonstre o passo a passo, do processo de mistura,
+de um array sendo ordenado, utilizando merge sort.
+Comece o passo a passo a partir da linha abaixo:
+
+
+ 73    5 4    6 8    2 1
+"""
+
+# solução:
+
+# 7   3   5   4   6   8   2   1
+# 37    45    68   12
+# 3 4 5 7     1 2 6 8
+# 1 2 3 4 5 6 7 8
