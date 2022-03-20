ac7fb39c7312039a7daa7b2b00c9c0cbe002613f
Savio Moraes <smmsavio@hotmail.com>
2022-03-19 21:27:19 -0300

    exercicio 6 do bloco 35.3

---
+++ Modulo 4/Bloco_35-Algoritmos/Dia_03-Algoritmos_de_Ordenação_e_Busca/Exercícios/exercicio6.py
@@ -0,0 +1,42 @@
+"""
+Exercício 6:Para descobrirmos qual commit introduziu um erro no sistema,
+precisamos voltar no tempo verificando os commits antigos, de modo a descobrir
+um commit em que os testes falham. Supondo que isto será representado como um
+array de booleanos , descubra o índice onde o erro ocorreu pela primeira vez.
+Como os testes demoram a rodar, resolva o problema rodando o mínimo de testes
+possíveis.
+
+entrada: [True, True, True, True, False, False, False]
+saída: 4
+
+
+entrada: [True, True, False, False, False, False, False]
+saída: 2
+"""
+
+
+def find_first_bad_version(array):
+    low_index = 0
+    high_index = len(array) - 1
+
+    while high_index >= low_index:
+
+        middle_index = (high_index + low_index) // 2
+
+        if array[middle_index] is False:
+            high_index = middle_index - 1
+        else:
+            low_index = middle_index + 1
+
+    return low_index
+
+
+array1 = [True, True, True, True, False, False, False]
+array2 = [True, True, False, False, False, False, False]
+
+print("Saída array1:", find_first_bad_version(array1))
+print("Saída array2:", find_first_bad_version(array2))
+
+
+# 💡 Curiosidade: O comando git bisect executa de maneira similar a
+# este exercício, se implementado de forma eficiente 😂.
