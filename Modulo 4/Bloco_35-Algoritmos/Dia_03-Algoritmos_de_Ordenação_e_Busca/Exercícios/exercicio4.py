4e7740b60651a5c8320cdeb2f078825a429fdbf2
Savio Moraes <smmsavio@hotmail.com>
2022-03-19 21:23:41 -0300

    exercicio 5 do bloco 35.3

--- Modulo 4/Bloco_35-Algoritmos/Dia_03-Algoritmos_de_Ordenação_e_Busca/Exercícios/exercicio4.py
+++ Modulo 4/Bloco_35-Algoritmos/Dia_03-Algoritmos_de_Ordenação_e_Busca/Exercícios/exercicio4.py
@@ -65,3 +65,13 @@ for algorithm in (bubble_sort, merge_sort):
 
     with Cronometro(algorithm_name):
         algorithm(numbers)
+
+
+"""
+No bubble sort em um caso médio temos uma complexidade de O(n²), o que
+significa que normalmente temos n² trocas, já o merge sort , como utiliza
+dividir e conquistar, consegue ter uma complexidade O(n log n) em média.
+Mesmo em um pior cenário, onde ao embaralharmos os dois arrays, tornando-os
+inversamente ordenados, o merge sort seria mais rápido, pois sua complexidade
+ainda seria O(n log n) e o bubble sort teria complexidade quadrática O(n²).
+"""
