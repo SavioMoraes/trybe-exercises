e556ffcaec42b97183ca0472bb7dd6226defb2b0
Savio Moraes <smmsavio@hotmail.com>
2022-03-23 15:37:00 -0300

    quando duas chaves diferentes resultam no mesmo address

--- Modulo 4/Bloco_36-Estrutura_de_Dados_I-Arrays_Hashmaps_Sets/Dia_03-Hashmap_e_Dict/Quando_Duas_Chaves_Diferentes_Acessam_o_mesmo_Hashing/exemplo_separate_chaining.py
+++ Modulo 4/Bloco_36-Estrutura_de_Dados_I-Arrays_Hashmaps_Sets/Dia_03-Hashmap_e_Dict/Quando_Duas_Chaves_Diferentes_Acessam_o_mesmo_Hashing/exemplo_separate_chaining.py
@@ -6,18 +6,25 @@ class Employee:
 
 class HashMap:
     def __init__(self):
-        self._buckets = [None for i in range(10)]
+        # self._buckets = [None for i in range(10)]  # gerando ambiguidade
+        self._buckets = [[] for i in range(10)]  # searate chaining
 
     def get_address(self, id_num):
         return id_num % 10
 
     def insert(self, employee):
         address = self.get_address(employee.id_num)
-        self._buckets[address] = employee
+        # self._buckets[address] = employee  # gerando ambiguidade
+        self._buckets[address].append(employee)  # searate chaining
 
     def get_value(self, id_num):
+        # address = self.get_address(id_num)  # gerando ambiguidade
+        # return self._buckets[address].name
         address = self.get_address(id_num)
-        return self._buckets[address].name
+        for item in self._buckets[address]:
+            if item.id_num == id_num:
+                return item.name
+        return None
 
     def has(self, id_num):
         address = self.get_address(id_num)
@@ -31,23 +38,25 @@ class HashMap:
 
 if __name__ == "__main__":
 
-    employees = [(14, "name1"), (23, "name2"), (10, "name3"), (9, "name4")]
+    employees = [
+        (14, "name1"),
+        (23, "name2"),
+        (10, "name3"),
+        (9, "name4"),
+        (24, "name1"),
+    ]
+    # employees = []
     registry = HashMap()
 
     for id_num, name in employees:
         employee = Employee(id_num, name)
         registry.insert(employee)
 
-    print(registry.get_value(23))
-    print(employees)
-    registry.update_value(23, "xablau")
-    print(registry.get_value(23))
-    print(employees)
+    # print(registry.get_value(23))
+    # print(employees)
     # print(registry.get_value(14))
     # print(employees)
-    # print(registry.get_value(24))  # acessou o mesmo queo 14
-    # print(employees)
-    print(registry.get_value(10))
-    registry.update_value(10, "name30")
-    print(registry.get_value(10))
+    print(registry.get_value(24))  # acessou o mesmo queo 14
     print(employees)
+    # registry.insert(24)
+    # print(type(id_num))
