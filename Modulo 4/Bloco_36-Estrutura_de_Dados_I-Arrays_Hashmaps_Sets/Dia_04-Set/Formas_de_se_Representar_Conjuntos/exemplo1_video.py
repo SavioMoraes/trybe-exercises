1ea3a2356ccbd863c1296b91fc5b8a36d285c392
Savio Moraes <smmsavio@hotmail.com>
2022-03-24 11:30:38 -0300

    finaliza formas de se representar conjuntos

--- Modulo 4/Bloco_36-Estrutura_de_Dados_I-Arrays_Hashmaps_Sets/Dia_04-Set/Formas_de_se_Representar_Conjuntos/exemplo1_video.py
+++ Modulo 4/Bloco_36-Estrutura_de_Dados_I-Arrays_Hashmaps_Sets/Dia_04-Set/Formas_de_se_Representar_Conjuntos/exemplo1_video.py
@@ -1,5 +1,32 @@
+# código inviável no caso de arrays muito grandes:
+
 A = [False for i in range(5)]
+print(A)
 A[1] = True
 A[2] = True
 A[3] = True
 print(A)
+print(A[0] is True)
+print(A[1] is True)
+
+# -----------------------------------------------------------------------
+print("-----------------------------------------------------------------")
+
+# com Dict:
+
+D = {1: True, 2: True, 3: True}
+print(D)
+print(1 in D)
+print(0 in D)
+
+# -----------------------------------------------------------------------
+print("-----------------------------------------------------------------")
+
+# com chaves em forma de string:
+
+S = {"Teste": True, 2: True, 3: True}
+print(S)
+print("Teste" in S)
+print("teste" in S)  # testa case sensitive
+print("Testesss" in S)  # testa palavra diferente da chave
+print(1 in S)  # testa uma chave inexistente
